bfb617dbf171f00e677516611ba7c4ac15614eda
Umit Yildirim <umity@uia.no>
2023-06-15 14:05:24 +0200

    proje5

---
+++ Proje5/app.js
@@ -0,0 +1,7 @@
+const main = document.querySelector(".grid-container")
+
+let screen = document.querySelector(".screen")
+
+main.addEventListener ("click", (e) => {
+   console.log(e.target.contains)
+} )
