f5ae7e91f335c33ed29f052a5188601e836cc12e
Umit Yildirim <umity@uia.no>
2023-06-15 14:12:32 +0200

    includes

--- Proje5/app.js
+++ Proje5/app.js
@@ -1,7 +1,12 @@
 const main = document.querySelector(".grid-container")
-
-let screen = document.querySelector(".screen")
-
-main.addEventListener ("click", (e) => {
-   console.log(e.target.contains)
-} )
+const screen = document.querySelector(".screen")
+main.addEventListener("click", (e) => {
+    // screen.innerText +=
+    let greys = ["0","1","2","3","4","5","6","7","8","9","."]
+    let oranges = ["+","-","X","÷","%"]
+    console.log(e.target.innerText);
+    // e.target.innerText == "2"
+    if (greys.includes(e.target.innerText)){
+        screen.innerText += e.target.innerText
+    }
+})
