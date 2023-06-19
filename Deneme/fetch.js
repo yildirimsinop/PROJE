bd288e3c4c1bb0e5c9d3d3c825bc8a85289c2463
Umit Yildirim <umity@uia.no>
2023-06-19 02:43:51 +0200

    albums

--- Deneme/fetch.js
+++ Deneme/fetch.js
@@ -1,20 +1,28 @@
-fetch ("settings.json").then(response => {
-    return response.json()
-}
-).then (responseJson => {
-    console.log(responseJson);
-    console.log(responseJson.password);
-})
+// fetch ("settings.json").then(response => {
+//     return response.json()
+// }
+// ).then (responseJson => {
+//     console.log(responseJson);
+//     console.log(responseJson.password);
+// })
 
-let userList = document.getElementById ("userName")
-fetch ("https://jsonplaceholder.typicode.com/albums").then (response => 
-response.json()
-).then (responseJson => {
-    console.log(responseJson);
+
+// ? ************* Fetch ile API'den veri alma *********
+// let userList = document.getElementById ("userName")
+// fetch ("https://jsonplaceholder.typicode.com/albums").then (response => 
+// response.json()
+// ).then (responseJson => {
+//     console.log(responseJson);
     
-    responseJson.forEach(item => {
-        let pDom = document.createElement ("p")
-        pDom.innerHTML = item.id
-        userList.appendChild(pDom)
-    });
+//     responseJson.forEach(item => {
+//         let pDom = document.createElement ("p")
+//         pDom.innerText = item.id
+//         userList.appendChild(pDom)
+//     });
+// })
+
+fetch ("https://jsonplaceholder.typicode.com/todos").then (response => {
+    response.json()
+}).then (data => {
+    console.log(data);
 })
