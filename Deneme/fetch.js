194fc1e49b5c705cc8339fa19df03a9464151511
Umit Yildirim <umity@uia.no>
2023-06-19 02:34:59 +0200

    json

--- Deneme/fetch.js
+++ Deneme/fetch.js
@@ -6,3 +6,15 @@ fetch ("settings.json").then(response => {
     console.log(responseJson.password);
 })
 
+let userList = document.getElementById ("userName")
+fetch ("https://jsonplaceholder.typicode.com/albums").then (response => 
+response.json()
+).then (responseJson => {
+    console.log(responseJson);
+    
+    responseJson.forEach(item => {
+        let pDom = document.createElement ("p")
+        pDom.innerHTML = item.id
+        userList.appendChild(pDom)
+    });
+})
