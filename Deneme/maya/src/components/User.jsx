8cb10d4a749e642ca8f6ea4ae802356009559e34
Umit Yildirim <umity@uia.no>
2023-06-26 01:21:31 +0200

    friends

--- Deneme/maya/src/components/User.jsx
+++ Deneme/maya/src/components/User.jsx
@@ -1,11 +1,23 @@
-function User ({name, surname, isLoggedIn, age}) {
+function User ({name, surname, isLoggedIn, age, friends}) {
     // console.log(props)
 
   return (  
+    <>
   <h1>
     {isLoggedIn ? `${name} ${surname} (${age})` : "Giris Yapmadiniz"}
     
  </h1>
+
+ {friends.map((friend, index)=> (
+
+    <div key = {index}>
+        {index}. {friend}
+    </div>
+
+ ))
+ }
+    
+    </>
 )
 }
 
