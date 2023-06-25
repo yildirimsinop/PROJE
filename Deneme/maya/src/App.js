506e02843eb56d120df105e14b2534e8a2c40cbd
Umit Yildirim <umity@uia.no>
2023-06-26 01:02:39 +0200

    user

--- Deneme/maya/src/App.js
+++ Deneme/maya/src/App.js
@@ -3,7 +3,13 @@ import User from "./components/User"
 function App() {
   return (
     <>
-      <User name= "Mehmet" surname = "yilmaz" isLoggedIn = {true} age= {29}/>
+      <User 
+      name= "Mehmet" 
+      surname = "yilmaz" 
+      isLoggedIn = {true} 
+      age= {29}
+      friends = {["Ahmet", "Tayfun", "Gokhan","Ayse","Fatma",]}
+      />
     </>
   )
 }
