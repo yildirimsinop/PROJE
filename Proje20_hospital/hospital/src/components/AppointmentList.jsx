b6d141a8923705520940f6856ccc9bfbc2ef9bf4
Umit Yildirim <umity@uia.no>
2023-07-11 22:00:06 +0200

    chat

--- Proje20_hospital/hospital/src/components/AppointmentList.jsx
+++ Proje20_hospital/hospital/src/components/AppointmentList.jsx
@@ -3,17 +3,19 @@ const AppointmentList = () => {
   return (
     <Stack
       direction="horizontal"
-      className="d-flex justify-content-around bg-secondary"
+      className="d-flex justify-content-around bg-secondary text-light"
       gap={3}
     >
       <div>
         <div className="p-2">Patient Name</div>
         <div className="p-2">Doctor Name</div>
       </div>
+
       <div>
         <div className="p-2 ms-auto">Consulted</div>
         <div className="p-2">Date</div>
       </div>
+
       <div className="p-2">Delete Icon</div>
     </Stack>
   );
