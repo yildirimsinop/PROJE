6cc48324dce05beda469db29f8212b0567f0a0a3
Umit Yildirim <umity@uia.no>
2023-07-14 14:28:00 +0200

    fix:hospital

--- appointment-app-starter/src/components/Doctors.jsx
+++ appointment-app-starter/src/components/Doctors.jsx
@@ -1,4 +1,7 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import { doctorData } from "../helper/data";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Doctors = () => {
   return (
@@ -6,8 +9,17 @@ const Doctors = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row>
+        {doctorData.map((item) => (
+          <Col>
+            <img src={item.img} alt="" />
+            <h3>{item.name}</h3>
+            <h4>{item.dep}</h4>
+          </Col>
+        ))}
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
