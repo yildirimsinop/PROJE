c1d14053d076ce1a80d30f6f549166fdcb0163a8
Umit Yildirim <umity@uia.no>
2023-07-15 12:07:02 +0200

    event

--- Interview/src/components/Question.jsx
+++ Interview/src/components/Question.jsx
@@ -1,22 +1,24 @@
 import Accordion from "react-bootstrap/Accordion";
+import Data from "../hjelper/data";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Question = () => {
+  console.log(Data);
   return (
-    <div>
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+    <div className="justify-content-center">
+      {Data.map((item, index) => (
+        <Row key={item.id} className="mb-3">
+          <Col md={{ span: 4, offset: 4 }}>
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey={index.toString()}>
+                <Accordion.Header>{item.question}</Accordion.Header>
+                <Accordion.Body>{item.answer}</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+        </Row>
+      ))}
     </div>
   );
 };
