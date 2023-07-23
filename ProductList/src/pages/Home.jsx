bcf1667d05e6da8566f2f1934bf3281d7bb81afd
Umit Yildirim <umity@uia.no>
2023-07-23 14:23:26 +0200

    category

--- ProductList/src/pages/Home.jsx
+++ ProductList/src/pages/Home.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import ProductCard from "../components/ProductCard";
 import axios from "axios";
 
 const Home = () => {
-  const data = async () => {
+  const [product, UseProduct] = useState([]);
+  const [category, UseCategory] = useState([]);
+
+  const dataProduct = async () => {
     const dataUrl = " https://fakestoreapi.com/products";
-    let res = await axios.get(dataUrl);
-    let { data } = res.data;
-    console.log(data);
+
+    try {
+      // const res = await axios(dataUrl)
+      // setTutorials(res.data)
+      const { data } = await axios.get(dataUrl);
+      UseProduct(data);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log({ dataProduct });
   };
+
+  useEffect(() => {
+    dataProduct();
+  }, []);
+
+  const categoryData = async () => {
+    const categoryUrl = " https://fakestoreapi.com/products/categories";
+
+    try {
+      // const res = await axios(categoryUrl)
+      // setTutorials(res.data)
+      const { category } = await axios.get(categoryUrl);
+      UseCategory(category);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log({ category });
+  };
+
+  useEffect(() => {
+    categoryData();
+  }, []);
   return (
     <div>
-      <Header />
-      <ProductCard />
+      <Header dataProduct={dataProduct} />
+      <ProductCard categoryData={categoryData} />
     </div>
   );
 };
