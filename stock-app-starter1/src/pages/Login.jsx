1939ae3fb3a38c8a986b1590a8cb2c2c304cf544
Umit Yildirim <umity@uia.no>
2023-08-17 13:32:24 +0200

    login

--- stock-app-starter1/src/pages/Login.jsx
+++ stock-app-starter1/src/pages/Login.jsx
@@ -17,94 +17,116 @@ const Login = () => {
 
   //? harici validasyon semasi
   const loginSchema = object({
-    email: string().email("Lutfen valid bir email giriniz").required(),
-    password: string().required(),
+    email: string()
+      .email("Please enter a valid email")
+      .required("This field is required."),
+    password: string()
+      .required("This field is required.")
+      .min(8, "At least 8 characters must be entered.")
+      .max(16, "A maximum 16 characters must be entered.")
+      .matches(/\d+/, "must be at least 1 digit.")
+      .matches(/[a-z]/, "must be at least 1 lowercase letter.")
+      .matches(/[A-Z]/, "must be at least 1 Uppercase letter.")
+      .matches(
+        /[!,?{}><%&$#£+-.]/,
+        "must contain at least 1 special character."
+      ),
   });
+
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <Grid item xs={12} mb={3}>
+          <Typography variant="h3" color="primary" align="center">
+            STOCK APP
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.light",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={4}
+            color="secondary.light"
+          >
+            Login
+          </Typography>
+
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginSchema}
+            onSubmit={(values, action) => {
+              //TODO login(values) POST islemi
+              action.resetForm();
+              action.setSubmitting(false);
+            }}
+          >
+            {({ handleChange, handleBlur, values, touched, errors }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={errors.email}
+                  />
+                  <TextField
+                    label="password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={errors.password}
+                  />
+                  <Button variant="contained" type="submit">
+                    Submit
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
+
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/register">Do you have not an account?</Link>
+          </Box>
+        </Grid>
+
+        <Grid item xs={10} sm={7} md={6}>
+          <Container>
+            <img src={image} alt="img" />
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
-
-return (
-  <Container maxWidth="lg">
-    <Grid
-      container
-      justifyContent="center"
-      direction="row-reverse"
-      sx={{
-        height: "100vh",
-        p: 2,
-      }}
-    >
-      <Grid item xs={12} mb={3}>
-        <Typography variant="h3" color="primary" align="center">
-          STOCK APP
-        </Typography>
-      </Grid>
-
-      <Grid item xs={12} sm={10} md={6}>
-        <Avatar
-          sx={{
-            backgroundColor: "secondary.light",
-            m: "auto",
-            width: 40,
-            height: 40,
-          }}
-        >
-          <LockIcon size="30" />
-        </Avatar>
-        <Typography variant="h4" align="center" mb={4} color="secondary.light">
-          Login
-        </Typography>
-
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={loginSchema}
-          onSubmit={(values, action) => {
-            //TODO login(values) POST islemi
-            action.resetForm();
-            action.setSubmitting(false);
-          }}
-        >
-          {({ handleChange, handleBlur, values, touched, errors }) => (
-            <Form>
-              <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                <TextField
-                  label="Email"
-                  name="email"
-                  id="email"
-                  type="email"
-                  variant="outlined"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  error={touched.email && Boolean(errors.email)}
-                  helperText={touched.email && errors.email}
-                />
-                <TextField
-                  label="password"
-                  name="password"
-                  id="password"
-                  type="password"
-                  variant="outlined"
-                />
-                <Button variant="contained" type="submit">
-                  Submit
-                </Button>
-              </Box>
-            </Form>
-          )}
-        </Formik>
-
-        <Box sx={{ textAlign: "center", mt: 2 }}>
-          <Link to="/register">Do you have not an account?</Link>
-        </Box>
-      </Grid>
-
-      <Grid item xs={10} sm={7} md={6}>
-        <Container>
-          <img src={image} alt="img" />
-        </Container>
-      </Grid>
-    </Grid>
-  </Container>
-);
 
 export default Login;
