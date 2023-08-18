598af4df7079dfea9174049dbb9d09074d3ff159
Umit Yildirim <umity@uia.no>
2023-08-18 22:13:56 +0200

    url

--- stock-app-starter1/src/components/MenuListItems.jsx
+++ stock-app-starter1/src/components/MenuListItems.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
+
+const icon = [{
+    icon:<SpaceDashboardIcon/>,
+    title:"Dashboard",
+    url:,
+}]
 
 const MenuListItems = () => {
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 };
 
 export default MenuListItems;
