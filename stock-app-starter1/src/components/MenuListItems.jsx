97d2366d599e0848eb20496dc14b0115596d64b6
Umit Yildirim <umity@uia.no>
2023-08-18 22:35:34 +0200

    change

--- stock-app-starter1/src/components/MenuListItems.jsx
+++ stock-app-starter1/src/components/MenuListItems.jsx
@@ -1,28 +1,66 @@
-import React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import { useNavigate } from "react-router-dom";
 
 const icons = [
   {
-    icon: <SpaceDashboardIcon />,
+    icon: <DashboardCustomizeIcon />,
     title: "Dashboard",
     url: "/stock/",
+  },
+  {
+    title: "Purchase",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+  {
+    title: "Admin Panel",
+    icon: <SupervisorAccountIcon />,
+    url: "https://10001.fullstack.clarusway.com/admin",
   },
 ];
 
 const MenuListItems = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      {" "}
       <List>
         {icons.map((item, index) => (
-          <ListItem key={index} disablePadding>
+          <ListItem
+            key={index}
+            disablePadding
+            onClick={() => navigate(item.url)}
+          >
             <ListItemButton>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.title} />
