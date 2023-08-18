d49dee5824e860801be53cbb0137268ab5291370
Umit Yildirim <umity@uia.no>
2023-08-18 21:59:47 +0200

    menulist

---
+++ stock-app-starter1/src/components/MenuListItems.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuListItems = () => {
+  return <div></div>;
+};
+
+export default MenuListItems;
--- stock-app-starter1/src/pages/Dashboard.jsx
+++ stock-app-starter1/src/pages/Dashboard.jsx
@@ -30,18 +30,6 @@ function Dashboard(props) {
     <div>
       <Toolbar />
       <Divider />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
     </div>
   );
 
