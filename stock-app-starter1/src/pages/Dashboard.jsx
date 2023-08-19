5bbf08e81fbcb57b33fc4f07b821286a725ad6f0
Umit Yildirim <umity@uia.no>
2023-08-19 11:54:44 +0200

    drawer

--- stock-app-starter1/src/pages/Dashboard.jsx
+++ stock-app-starter1/src/pages/Dashboard.jsx
@@ -84,6 +84,7 @@ function Dashboard(props) {
             "& .MuiDrawer-paper": {
               boxSizing: "border-box",
               width: drawerWidth,
+              backgroundColor: "secondary.main",
             },
           }}
         >
