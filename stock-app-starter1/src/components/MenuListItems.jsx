d88c12f06aabcd7543d1bc8acf7e09697d74f93c
Umit Yildirim <umity@uia.no>
2023-08-19 12:34:46 +0200

    item

--- stock-app-starter1/src/components/MenuListItems.jsx
+++ stock-app-starter1/src/components/MenuListItems.jsx
@@ -52,6 +52,8 @@ const icons = [
 
 const MenuListItems = () => {
   const navigate = useNavigate();
+
+  //? window.location.href = item.url
   return (
     <div>
       <List>
@@ -59,7 +61,11 @@ const MenuListItems = () => {
           <ListItem
             key={index}
             disablePadding
-            onClick={() => navigate(item.url)}
+            onClick={() => {
+              item.url.includes("http" || "wwww")
+                ? window.open(item.url, "_blank")
+                : navigate(item.url);
+            }}
             sx={{
               color: "white",
               ".MuiSvgIcon-root": { color: "white" },
