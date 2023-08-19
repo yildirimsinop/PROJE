82546337eaca52091fdf68cca6b0a19a47f21638
Umit Yildirim <umity@uia.no>
2023-08-19 14:22:01 +0200

    getFirms

--- stock-app-starter1/src/pages/Firms.jsx
+++ stock-app-starter1/src/pages/Firms.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Button, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Firms = () => {
-  return <div></div>;
+  const { token } = useSelector((state) => state.auth);
+  const getFirms = async () => {
+    try {
+      const { data } = await axios(
+        `${import.meta.env.VITE_BASE_URL}/stock/firms/`,
+        {
+          headers: { Authorization: `Token ${token} ` },
+        }
+      );
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color={"error"} mb={3}>
+        Firms
+      </Typography>
+      <Button variant="contained">NEW FIRM</Button>
+    </div>
+  );
 };
 
 export default Firms;
