a7605c5ec63dfa0307650f47ffd55a771b0c7bc9
Umit Yildirim <umity@uia.no>
2023-08-20 11:36:08 +0200

    stock slice

--- stock-app-starter1/src/pages/Firms.jsx
+++ stock-app-starter1/src/pages/Firms.jsx
@@ -2,10 +2,15 @@ import { Button, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { fetchFail, fetchStart } from "../features/authSlice";
+import { useDispatch } from "react-redux";
 
 const Firms = () => {
   const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   const getFirms = async () => {
+    dispatch(fetchStart());
     try {
       const { data } = await axios(
         `${import.meta.env.VITE_BASE_URL}/stock/firms/`,
@@ -14,8 +19,11 @@ const Firms = () => {
         }
       );
 
+      dispatch(fetFirmSuccess(data));
+
       console.log(data);
     } catch (error) {
+      dispatch(fetchFail());
       console.log(error);
     }
   };
